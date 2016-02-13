715cd51e8e4c873b4b39f73d900c728cb8d2e132
Dylan Fischler <dylanfischler@gmail.com>
2016-02-13 15:09:36 -0500

    adding a static go server

--- server/server.go
+++ server/server.go
@@ -3,12 +3,14 @@ package main
 import (
   "log"
   "net/http"
+  "github.com/gorilla/mux"
 )
 
-func main(){
-  fs := http.FileServer(http.Dir("dist"))
-  http.Handle("/", fs)
-
-  log.Println("Listening...")
-  http.ListenAndServe(":8080", nil)
+func main() {
+	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("dist/")))
+	http.Handle("/", r)
+	
+	log.Println("Listening...")
+	http.ListenAndServe(":8080", nil)
 }
