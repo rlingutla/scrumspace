7000c4bebe465bebb5fc585a76cac7d0b168bb97
Dylan Fischler <dylanfischler@gmail.com>
2016-02-13 22:09:12 -0500

    updated Go server with support for browserHistory and static files

--- server/server.go
+++ server/server.go
@@ -3,14 +3,16 @@ package main
 import (
   "log"
   "net/http"
-  "github.com/gorilla/mux"
 )
 
+func defHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "dist/index.html")
+}
+
 func main() {
-	r := mux.NewRouter()
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("dist/")))
-	http.Handle("/", r)
-	
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("dist/"))))
+	http.HandleFunc("/", defHandler)
+
 	log.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
 }
