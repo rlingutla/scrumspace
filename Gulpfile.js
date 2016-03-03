6e228f21a8c1542188f3ccde9c3f9337031ed6b3
Dylan Fischler <dylanfischler@gmail.com>
2016-03-03 16:44:22 -0500

    meh, try #2

--- Gulpfile.js
+++ Gulpfile.js
@@ -44,10 +44,15 @@ gulp.task('js', function(){
 
   return gulp.src('src/client/entry.js')
     .pipe(webpack(require('./webpack.config.js'), null, function(err, stats){
-      if(err) throw new gutil.PluginError("webpack", err);
-      gutil.log("[webpack]", stats.toString({ 
-        colors: true, hash: false, timings: false, chunks: false, chunkModules: false, modules: false, children: true, version: true, cached: false, cachedAssets: false, reasons: false, source: false, errorDetails: false
-      }));
+      if(err) {
+        throw new gutil.PluginError("webpack", err);
+        process.exit(1);
+      }
+      else {
+        gutil.log("[webpack]", stats.toString({ 
+          colors: true, hash: false, timings: false, chunks: false, chunkModules: false, modules: false, children: true, version: true, cached: false, cachedAssets: false, reasons: false, source: false, errorDetails: false
+        }));
+      }
     }))
     .pipe(gulp.dest('dist/js/'))
     .pipe(livereload());
