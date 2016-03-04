4ebdf25f63effca666b0c1c76c72ccfabb1e9446
Dylan Fischler <dylanfischler@gmail.com>
2016-03-03 20:06:56 -0500

    some initial scrum board stuff

--- src/app/screens/Project/screens/Detail/components/Project.js
+++ src/app/screens/Project/screens/Detail/components/Project.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
 import Tab from './Tabs/Tab';
-import Board from './Board';
+import BoardView from './BoardView';
 
 /* Project details on a particular project*/
 class Project extends React.Component {
@@ -26,7 +26,7 @@ class Project extends React.Component {
 				<Nav active-tab={this.state.activeTab} tab-change={this.onTabChange} {...this.props}/>
 				<div id="tab-container">
 					<Tab tab-id={0} active-tab={this.state.activeTab}>
-						<Board />
+						<BoardView />
 					</Tab>
 					<Tab tab-id={1} active-tab={this.state.activeTab}>
 						Tab2
