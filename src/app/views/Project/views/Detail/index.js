b8f8cff82921aa0753e307b503d7e02f0d7012af
Abhay Vatsa <avatsa@umass.edu>
2016-03-06 15:03:20 -0500

    update

--- src/app/views/Project/views/Detail/index.js
+++ src/app/views/Project/views/Detail/index.js
@@ -6,9 +6,7 @@ import { Glyphicon, Button } from 'react-bootstrap';
 export default (props) => {
 	return (
 		<div id="content">
-			<TopNav view="Projects" {...props}> 
-				<Button>Create Project <Glyphicon glyph="plus" /></Button>
-			</TopNav>
+			<TopNav view="Project" {...props} />
 			<ProjectContainer id={props.params.id} />
 		</div>
 	);
