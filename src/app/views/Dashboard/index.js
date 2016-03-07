4e7eb786075a3e2bdb35d687c17106548f048ab0
Abhay Vatsa <avatsa@umass.edu>
2016-03-06 21:52:36 -0500

    refactored to contain container in index

--- src/app/views/Dashboard/index.js
+++ src/app/views/Dashboard/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Container from './containers';
 
 import TopNav from '../../shared/components/TopNav';
 import {
@@ -8,12 +7,49 @@ import {
 	Statistics
 } from './components';
 
+function isInSprint (project) {
+	return project.status === 'sprint';
+}
+
+function isActionable(task) {
+	return task.status === 'DOING' || task.status === 'BLOCKED';
+}
+
+const setActionableTasks = (project)  => {
+	project.actionableTasks = project.stories.map((e) => {
+		return e.tasks;
+	}).reduce((a, b) => a.concat(b).filter(isActionable));
+};
+
+const mapStateToProps = (state) => {
+	return state;
+};
+
+const mergeProps = (state, dispatchProps, ownProps) => {
+	let projects = state.projects.filter(isInSprint);
+	projects.forEach(setActionableTasks);
+	return projects;
+};
+
+// maps any actions this component dispatches to component props
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+const Container = (Dashboard) => {
+	return connect(
+	  mapStateToProps,
+	  mapDispatchToProps,
+	  mergeProps
+	)(Dashboard);
+};
+
 const Dashboard = (props) => {
 	return (
 		<div id="content">
 			<TopNav view="Dashboard"/>
 			<Statistics projects={props.projects}/>
-			{ props.projects.map((project, i) => <Project key={i} project={project} />)}
+			{ props.projects.map((project, i) => <Project key={i} project={project} />) }
 		</div>
 	);
 };
