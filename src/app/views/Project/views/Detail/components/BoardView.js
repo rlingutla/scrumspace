0baacf44142edff4376e31380fed538a1bf692ac
Dylan Fischler <dylanfischler@gmail.com>
2016-03-07 17:55:08 -0500

    progress bar stuff

--- src/app/views/Project/views/Detail/components/BoardView.js
+++ src/app/views/Project/views/Detail/components/BoardView.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ProgressBar from './ProgressBar';
+import ProgressBarContainer from './ProgressBar';
 import ScrumBoard from './ScrumBoard/ScrumBoard';
 
 const BoardView = (props) => {
 	return (
 		<div className="content">
 			<div className="project-info container-fluid">
-				<ProgressBar />
+				<ProgressBarContainer project_id={props._id} />
 			</div>
 			<ScrumBoard {...props} />
 		</div>
--- src/app/views/Project/views/Detail/components/ProgressBar.js
+++ src/app/views/Project/views/Detail/components/ProgressBar.js
@@ -1,19 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { ProgressBar } from 'react-bootstrap';
+import TaskTypes from '../../../../../constants/taskTypes';
 
-const ProjectProgressBar = (props) => {
-	return (
-		<div>
-			<div className="row left-right-align progress-bar-details">
-			    <div className="col-md-6"><strong>1/7</strong> Tasks Complete</div>
-			    <div className="col-md-6"><strong>5</strong> Days Left of Sprint</div>
+class ProjectProgressBar extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	countType(taskType){
+		let total = 0, typeCount = 0;
+
+		this.props.stories.forEach((story) => {
+			story.tasks.forEach((task) => {
+				if(task.status == taskType) ++typeCount;
+				++total;
+			});
+		});
+		let calc = Math.floor((typeCount/total)*100);
+		return calc;
+	}
+
+
+	render(){
+		return (
+			<div>
+				<div className="row left-right-align progress-bar-details">
+				    <div className="col-md-6"><strong>1/7</strong> Tasks Complete</div>
+				    <div className="col-md-6"><strong>5</strong> Days Left of Sprint</div>
+				</div>
+				<ProgressBar>
+					<ProgressBar bsStyle="info" now={this.countType(TaskTypes.UNASSIGNED)} key={1} />
+					<ProgressBar bsStyle="warning"now={this.countType(TaskTypes.DOING)} key={2} />
+					<ProgressBar bsStyle="danger" now={this.countType(TaskTypes.BLOCKED)}key={3} />
+					<ProgressBar bsStyle="success" now={this.countType(TaskTypes.DONE)}key={4} />
+				</ProgressBar>
 			</div>
-			<div className="progress">
-			    <div className="progress-bar progress-bar-warning" style={{width: '28%'}}></div>
-			    <div className="progress-bar progress-bar-danger" style={{width: '14%'}}></div>
-			    <div className="progress-bar progress-bar-success" style={{width: '14%'}}></div>
-			</div>
-		</div>
-	);
+		);
+	}
 };
 
-export default ProjectProgressBar;
+//redux
+const mapStateToProps = (state) => {
+	return state;
+}
+
+function mergeProps(stateProps, dispatchProps, ownProps) {
+	let project = stateProps.projects.find((proj) => proj._id == ownProps.project_id);
+	return Object.assign(project, ownProps, dispatchProps);
+}
+
+//maps any actions this component dispatches to component props
+const mapDispatchToProps = (dispatch) => {
+  return {};
+}
+
+const ProjectProgressBarContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(ProjectProgressBar);
+
+export default ProjectProgressBarContainer;
