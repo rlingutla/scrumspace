88dda2f32ee5ad296fdf9ce4ba6d4b55ff4672f2
Rachana Lingutla <rlingutla@umass.edu>
2016-03-07 21:18:35 -0500

    fixed project build failure

--- src/app/views/Project/views/Master/components/NewProjModal.js
+++ src/app/views/Project/views/Master/components/NewProjModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, OverlayTrigger, Tooltip, Popover, Button, Input, ButtonInput } from 'react-bootstrap';
-import ProjectItem from './components/ProjectItem';
+import ProjectItem from './ProjectItem';
 
 export default class NewProjModal extends React.Component{
   constructor(props){
@@ -13,49 +13,49 @@ export default class NewProjModal extends React.Component{
   //   this.setState({inputs : inputs});
   // }
 
-  createNewProj(){
+  // createNewProj(){
+  //
+  //   <ProjectItem />;
+  // }
 
-    <ProjectItem />;
-  }
+  // handleChange(e){
+  //   e.preventDefault();
+  // // e.target is the React Virtual DOM target of the input event -- the
+  // // <textarea> element. The textarea's `value` is the entire contents of
+  // // what the user has typed in so far.
+  // this.setState({value: e.target.value});
 
-  handleChange(e){
-    e.preventDefault();
-  // e.target is the React Virtual DOM target of the input event -- the
-  // <textarea> element. The textarea's `value` is the entire contents of
-  // what the user has typed in so far.
-  this.setState({value: e.target.value});
-
-  }
+//  }
 
   render () {
     return (
       <div>
-      <Modal show={this.props.show} onHide={this.props.changeModal}>
+        <Modal show={this.props.show} onHide={this.props.changeModal}>
 
-        <Modal.Header closeButton>
-            <Modal.Title>Create a new project</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-           <h4><b>Enter Project Details</b></h4>
-          <form>
-          <Input type="text" label="Enter project name" placeholder="Name" valueName={this.state.value} onChange={(e) => this.handleChange(e)}/>
-          <Input type="text" label="Enter project description" placeholder="Description" valueDesc={this.state.value} onChange={(e) => this.handleChange(e)}/>
-          </form>
+          <Modal.Header closeButton>
+              <Modal.Title>Create a new project</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {/*
+            <h4><b>Enter Project Details</b></h4>
+            <form>
+            <Input type="text" label="Enter project name" placeholder="Name" valueName={this.state.value} onChange={(e) => this.handleChange(e)}/>
+            <Input type="text" label="Enter project description" placeholder="Description" valueDesc={this.state.value} onChange={(e) => this.handleChange(e)}/>
+            </form>
 
-          <h4><b>Enter Members</b></h4>
+            <h4><b>Enter Members</b></h4>
 
-          <form>
-          <Input type="text"  placeholder="Name" valueMember={this.state.value} onChange={(e) => this.handleChange(e)}/>
-          <Input type="text"  placeholder="Name" valueMember={this.state.value} onChange={(e) => this.handleChange(e)}/>
-          </form>
-          <Button bsStyle="primary" >Add members</Button>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button bsStyle="success" onClick={this.createNewProj()}>Create</Button>
-        </Modal.Footer>
-
-
-          </Modal>
+            <form>
+            <Input type="text"  placeholder="Name" valueMember={this.state.value} onChange={(e) => this.handleChange(e)}/>
+            <Input type="text"  placeholder="Name" valueMember={this.state.value} onChange={(e) => this.handleChange(e)}/>
+            </form>
+            <Button bsStyle="primary" >Add members</Button>
+            */}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="success">Create</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
