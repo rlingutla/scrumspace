7488b30c52f0d4c5d51203275e17e3ac0323715e
Rachana Lingutla <rlingutla@umass.edu>
2016-03-07 20:53:43 -0500

    Added New project creation modal

--- src/app/views/Project/views/Master/index.js
+++ src/app/views/Project/views/Master/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import TopNav from '../../../../shared/components/TopNav';
 import ProjectListContainer from './components/ProjectList';
-import { Button, Glyphicon } from 'react-bootstrap';
+import {Button,Glyphicon} from 'react-bootstrap';
+import NewProjModal from './components/NewProjModal';
 
 /* Master for all user projects */
-export default (props) => {
-	return (
-		<div id="content">
-			<TopNav view="Projects">
-				<Button>Create Project <Glyphicon glyph="plus" /></Button>
-			</TopNav>
-			<ProjectListContainer />
-		</div>
-	);
-};
+export default class Project extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {modalIsOpen:false};
+
+	}
+
+	handleClick(e){
+		e.preventDefault();
+    this.changeModal();
+	}
+
+	changeModal(){
+    this.setState({modalIsOpen : !this.state.modalIsOpen});
+	}
+
+	render(){
+		return (
+			<div id="content">
+				<TopNav view="Projects">
+					<Button onClick={this.handleClick.bind(this)}>Create Project <Glyphicon glyph="plus" /></Button>
+				</TopNav>
+				<NewProjModal show={this.state.modalIsOpen} changeModal={this.changeModal.bind(this)} />
+				<ProjectListContainer />
+			</div>
+		);
+	}
+
+}
