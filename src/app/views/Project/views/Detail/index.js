a4a231d629ad49af5a7ba361236a6bbb6a239f28
Dylan Fischler <dylanfischler@gmail.com>
2016-03-08 01:44:23 -0500

    added avatars and some board styling

--- src/app/views/Project/views/Detail/index.js
+++ src/app/views/Project/views/Detail/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import TopNav from '../../../../shared/components/TopNav';
-import ProjectContainer from './components/Project';
+import Project from './components/Project';
 import { Glyphicon, Button } from 'react-bootstrap';
 
 export default (props) => {
 	return (
 		<div id="content">
 			<TopNav view="Project" {...props} />
-			<ProjectContainer id={props.params.id} />
+			<Project id={props.params.id} />
 		</div>
 	);
 };
