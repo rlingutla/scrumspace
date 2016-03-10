7b818d7f0bf9832a2e00fcd51d4d70f436ff6fb3
Dylan Fischler <dylanfischler@gmail.com>
2016-03-09 20:40:05 -0500

    some design changes (not done)

--- src/app/mock_server/data.js
+++ src/app/mock_server/data.js
@@ -2,12 +2,12 @@ var initialData = {
 	loading: false,
 	users: {
 		0: {
-	'_id': 0,
-	'first_name': 'Donald',
-	'last_name': 'Trump',
-	'email': 'america@donaldjtrump.com',
-	'display_name': 'DJ Trump',
-	'password': 'd4866854120e8bb207d6f8e11fce8b99'
+			'_id': 0,
+			'first_name': 'Donald',
+			'last_name': 'Trump',
+			'email': 'america@donaldjtrump.com',
+			'display_name': 'DJ Trump',
+			'password': 'd4866854120e8bb207d6f8e11fce8b99'
 		}
 	},
 	projects: [
@@ -15,7 +15,16 @@ var initialData = {
 		'_id': 0,
 		'title': 'Drumpf',
 		'description': 'Make America Great Again',
-		'users': [0],
+		'users': [
+			{
+				'_id': 0,
+				'first_name': 'Donald',
+				'last_name': 'Trump',
+				'email': 'america@donaldjtrump.com',
+				'display_name': 'DJ Trump',
+				'password': 'd4866854120e8bb207d6f8e11fce8b99'
+			}
+		],
 		'status': 'sprint',
 		'current_sprint': 0,
 		'avatar': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg',
@@ -70,7 +79,16 @@ var initialData = {
 		'_id': 1,
 		'title': 'Feel the Bern',
 		'description': 'Fight the Power',
-		'users': [0],
+		'users': [
+			{
+				'_id': 0,
+				'first_name': 'Donald',
+				'last_name': 'Trump',
+				'email': 'america@donaldjtrump.com',
+				'display_name': 'DJ Trump',
+				'password': 'd4866854120e8bb207d6f8e11fce8b99'
+			}
+		],
 		'status': 'planning',
 		'current_sprint': null,
 		'avatar': 'http://www.slate.com/content/dam/slate/articles/news_and_politics/politics/2015/07/150706_POL_Sanders.jpg.CROP.promo-xlarge2.jpg',
