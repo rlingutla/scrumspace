266560b7cd6b3f133616ef4af4b41246f4b51d30
Dylan Fischler <dylanfischler@gmail.com>
2016-03-14 22:08:55 -0400

    autosuggest kinda actually works wooo

--- src/app/shared/components/SuggestionInput.js
+++ src/app/shared/components/SuggestionInput.js
@@ -4,18 +4,95 @@ import { search } from '../../mock_server/server';
 export default class SuggestionInput extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			suggestions: [],
+			value: "",
+			selected: 0,
+			display:false
+		}
 	}
 
 	handleChange(e){
 		let text = e.target.value;
 		let results = search(text, this.props.collection, this.props.searchKey);
-		console.log(results.map((res) => res[this.props.searchKey]));
+		this.setState({ value: text, suggestions: results, display: results.length > 0, selected: 0 });
 	}
+
+	handleFocus(e){
+		if(this.state.suggestions.length > 0){
+			this.setState({display:true});	
+		}
+	}
+
+	handleBlur(e){
+		//figure out how to check suggestion click
+		e.preventDefault();
+		//invalid input
+		// let text = (this.state.suggestions.indexOf(e.target.value) > -1) ? e.target.value:"";
+		// this.setState({ display:false, value: '', suggestions: [] });
+	}
+
+
+	// suggestion events
+	handleClick(e, sug){
+		this.setState({ value: sug[this.props.searchKey], display:false });
+	}
+	handleHover(e, sug, index){
+		this.setState({ selected: index });
+	}
+
+	//key events
+	handleKeyDown(e){
+		switch(e.keyCode){
+			//enter
+			case 13:
+				if(this.state.suggestions.length > 0){
+					e.target.blur();
+					this.setState({ value: this.state.suggestions[this.state.selected][this.props.searchKey], display:false });
+				}
+				break;
+			//down
+			case 40:
+				if(this.state.selected < this.state.suggestions.length - 1){
+					this.setState({ selected: ++this.state.selected });
+				}
+				break;
+			//up
+			case 38:
+				if(this.state.selected > 0){
+					this.setState({ selected: --this.state.selected });
+				}
+				break;
+		}
+
+	}
+
 
 	render(){
 		return (
 			<div className="suggestion-input">
-				<input onChange={(e) => this.handleChange(e)}/>
+				<input 
+					className="form-control" 
+					value={this.state.value}  
+					onChange={(e) => this.handleChange(e)} 
+					onFocus={(e) => this.handleFocus(e)}
+					onBlur={(e) => this.handleBlur(e)}
+					onKeyDown={(e) => this.handleKeyDown(e)}
+				/>
+
+				{(this.state.display) ? 
+				<div className="suggestions">
+					<ul>
+						{this.state.suggestions.map((sug, i) => 
+							<li className={(this.state.selected === i) ? 'selected':''} 
+								onClick={(e) => this.handleClick(e, sug)} 
+								onMouseEnter={(e) => this.handleHover(e, sug, i)} 
+								key={i}>
+								{sug[this.props.searchKey]}
+							</li>
+						)}
+					</ul>
+				</div>:null}
 			</div>
 		)
 	}
