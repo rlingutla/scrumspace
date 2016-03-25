814d9a829a5d2718b0eee946fda64c07097475f0
Niharika Venkatathri <nvenkata@umass.edu>
2016-03-25 10:59:50 -0400

    task detail modal updated

--- src/app/views/Project/views/Detail/components/ScrumBoard/TaskDetailModal.js
+++ src/app/views/Project/views/Detail/components/ScrumBoard/TaskDetailModal.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Modal, OverlayTrigger, Tooltip, Popover, Button, Input, ButtonInput } from 'react-bootstrap';
+import { Modal, OverlayTrigger, Tooltip, Popover, Button, Input, ButtonInput, Row, Col } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import TaskTypes from '../../../../../../constants/taskTypes';
 import TaskStatus from '../../../../shared/TaskStatus';
 
+
+const AssignedMember = (props) => {
+	return(
+		<span>{props.first_name} {props.last_name} </span>
+	)
+}
 
 export default class TaskDetailModal extends React.Component{
 
@@ -17,26 +23,26 @@ export default class TaskDetailModal extends React.Component{
 				<Modal show={this.props.isModalOpen} onHide={(e) => this.props.changeModal(e)} className={"task-detail " + this.props.status}>
 					<Modal.Header closeButton>
 						<Modal.Title>
-							<span className="task_id">{this.props._id} </span> 
+							<span className="task_id">{this.props._id} </span>
 							{this.props.description}
 							<span className="task-story">, from story {this.props.story_id}</span>
 						</Modal.Title>
 					</Modal.Header>
 					<Modal.Body>
 						<TaskStatus status={this.props.status} />
-						<br></br>
-						<div className="row left-right-align">
-							<div className="col-md-6">
-								<b>Assigned To</b>
-								<br></br>
-								<ul>
-									<button type="button" className="btn btn-default">
-										<span className="glyphicon glyphicon-trash"></span>
-									</button>
-								</ul>
-								<input type="text" className="form-control" id="inputMember" placeholder="Add Member"/>
-							</div>
-						</div>
+						<br/>
+						<Row>
+							<Col xs={8}>
+								<h4>Assigned To:</h4>
+								{this.props.assignedTo.map((user,i) => {
+									return <AssignedMember key={i} {...user} />
+								})}
+							</Col>
+							<Col xs={4} style={{textAlign:"right"}}>
+								<Button>Take Task</Button>
+								<Button bsStyle="danger">Delete Task</Button>
+							</Col>
+						</Row>
 					</Modal.Body>
 				</Modal>
 			</div>
