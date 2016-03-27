61c7c582ff88fe697dc23a8131422aaf00a4c4c5
Niharika Venkatathri <nvenkata@umass.edu>
2016-03-27 00:56:11 -0400

    multiselect, buttons for users, editable title

--- src/app/views/Project/views/Detail/components/ScrumBoard/TaskDetailModal.js
+++ src/app/views/Project/views/Detail/components/ScrumBoard/TaskDetailModal.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Modal, OverlayTrigger, Tooltip, Popover, Button, Input, ButtonInput, Row, Col } from 'react-bootstrap';
+import { Modal, OverlayTrigger, Tooltip, Popover, Button, Input, ButtonInput, Row, Col, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import TaskTypes from '../../../../../../constants/taskTypes';
 import TaskStatus from '../../../../shared/TaskStatus';
-
+import MultiSelect from '../../../../../../shared/components/MultiSelect';
 
 const AssignedMember = (props) => {
 	return(
@@ -17,6 +17,47 @@ export default class TaskDetailModal extends React.Component{
 		super(props);
 	}
 
+	setAssignedTo(members) {
+		this.setState({
+			assignedTo: members
+		});
+	}
+
+	toggleEdit(target, value){
+		let edit = Object.assign({}, this.state[target], { editing: value });
+		this.setState({
+			[target]: edit
+		});
+	}
+
+	handleBlur(target, e){
+		if(e.target.value){
+			this.toggleEdit(target, false);
+			let story = Object.assign({}, this.props, {[target]: e.target.value.trim()});
+			//update the story
+			this.props.updateStory(this.props.project_id, story);
+		}
+
+	}
+
+	handleChange(target, e){
+		let changed = Object.assign({}, this.state[target], { value: e.target.value });
+		this.setState({
+			[target]: changed
+		});
+	}
+
+	handleKeyDown(target, e){
+		switch(e.keyCode){
+			//enter key pressed
+			case 13:
+				if(!e.shiftKey){
+					e.target.blur();
+					this.handleBlur(target, e);
+				}
+		}
+	}
+
 	render(){
 		return (
 			<div className={"task-detail " + this.props.status}>
@@ -24,7 +65,18 @@ export default class TaskDetailModal extends React.Component{
 					<Modal.Header closeButton>
 						<Modal.Title>
 							<span className="task_id">{this.props._id} </span>
-							{this.props.description}
+
+						{this.props.description}
+
+						{(this.props.description.editing) ?
+							<input autoFocus
+								onChange={(e) => this.handleChange('title', e)}
+								value={this.props.description.value}
+								onBlur={(e) => this.handleBlur('title', e)}
+								onKeyDown={(e) => this.handleKeyDown('title', e)}
+							/>
+							:<h5 className="editable" onClick={(e) => this.toggleEdit('title', true)}>{this.props.description.value}</h5>
+						}
 							<span className="task-story">, from story {this.props.story_id}</span>
 						</Modal.Title>
 					</Modal.Header>
@@ -35,12 +87,20 @@ export default class TaskDetailModal extends React.Component{
 							<Col xs={8}>
 								<h4>Assigned To:</h4>
 								{this.props.assignedTo.map((user,i) => {
-									return <AssignedMember key={i} {...user} />
+									return (
+										<ButtonGroup horizontal>
+										<Button><AssignedMember key={i} {...user} /></Button>
+										<Button><span className="glyphicon glyphicon-remove"></span></Button>
+										</ButtonGroup>);
 								})}
+								<MultiSelect collection="users" labelKey="display_name" valueKey="_id" updateState={(members) => this.setAssignedTo(members)}/>
+								<Button><span className="glyphicon glyphicon-plus"></span></Button>
 							</Col>
 							<Col xs={4} style={{textAlign:"right"}}>
-								<Button>Take Task</Button>
-								<Button bsStyle="danger">Delete Task</Button>
+								<ButtonGroup vertical>
+									<Button>Take Task</Button>
+									<Button bsStyle="danger">Delete Task</Button>
+								</ButtonGroup>
 							</Col>
 						</Row>
 					</Modal.Body>
