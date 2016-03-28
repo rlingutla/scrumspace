a6ca49f1f60b04f0f190762d7b507d291b01bce1
Ryan Jerue <rjerue@umass.edu>
2016-03-28 19:54:41 -0400

    started redux stuff. this probably isn't alot of new stuff

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import projects from './projects';
 import user from './user';
 import settings from '../views/Settings/reducers/';
+import projectPlanning from '../views/Project/views/views/Planning/reducers/';
 
 const scrumApp = combineReducers({
 	user,
 	projects,
-	settings
+	settings,
+	projectPlanning
 });
 
 export default scrumApp;
