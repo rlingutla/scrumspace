ac97b35d43ac6ecce530e7ed821f5f242b6cca94
Abhay Vatsa <avatsa@umass.edu>
2016-03-28 22:32:00 -0400

    working reducer for settings

--- src/app/reducers/user.js
+++ src/app/reducers/user.js
@@ -1,8 +1,15 @@
-let initialState = {};
-
-const user = (state = initialState, action) => {
+const user = (state = {}, action) => {
 	switch (action.type){
-		default: //just returning state for now
+		case 'CHANGE_SETTINGS_STATE':
+			let newSettings = action.newSettings;
+			return Object.assign({
+				first_name: newSettings.first_name,
+				last_name: newSettings.last_name,
+				email: newSettings.email,
+				display_name: newSettings.display_name,
+				password: newSettings.password
+			});
+		default: 
 			return state;
 	}
 };
