ad1ee836d4eaeed57ae4ae41aeb2ab7b3b09b1aa
Abhay Vatsa <avatsa@umass.edu>
2016-03-29 22:48:35 -0400

    pics for team members

--- src/app/mock_server/data.js
+++ src/app/mock_server/data.js
@@ -22,7 +22,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Abhay Vatsa',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'http://www.abhayvatsa.com/img/abhay.png'
 		},
 		2: {
 			'_id': 2,
@@ -31,7 +31,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Dylan Fischler',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'http://www.dylanfischler.com/includes/me_bw.jpg'
 		},
 		3: {
 			'_id': 3,
@@ -40,7 +40,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Niha Venkatathri',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'http://umasscswomen.weebly.com/uploads/3/8/0/8/38087803/3410479.jpg?153'
 		},
 		4: {
 			'_id': 4,
@@ -49,7 +49,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Ryan Jerue',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAa7AAAAJGQ2NjlmY2MzLTgzYjctNGRhYy05ZmY2LTkxOGE3Mzg2NWU2MA.jpg'
 		},
 		5: {
 			'_id': 5,
@@ -58,7 +58,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Supriya Kankure',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAa1AAAAJDRjMTU3ZTA2LTFkMTEtNDM1MS04YjQ0LWI0YzRlOTExODQ3NA.jpg'
 		},
 		6: {
 			'_id': 6,
@@ -67,7 +67,7 @@ var initialData = {
 			'email': 'someemail',
 			'display_name': 'Rachana Lingutla',
 			'password': 'd4866854120e8bb207d6f8e11fce8b99',
-			'avatar_url': 'http://static1.businessinsider.com/image/55ca4540371d22462c8bcb17/donald-trump-is-still-soaring-in-iowa--but-there-are-now-some-clear-warning-signs.jpg'
+			'avatar_url': 'https://avatars.slack-edge.com/2016-02-07/20575719908_a1012b8fe4efe81099c3_72.png'
 		}
 	},
 	projects: [
