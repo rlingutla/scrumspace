694255424bfe383b8c7080e0c01f2f4f52a27964
Dylan Fischler <dylanfischler@gmail.com>
2016-04-01 09:49:23 -0400

    fixing some errors

--- src/app/views/Project/views/ProjectID/views/Settings/index.js
+++ src/app/views/Project/views/ProjectID/views/Settings/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import MultiSelect from 'app/shared/components/MultiSelect';
-import {Button, Row, Col } from 'react-bootstrap';
+import { Modal, Button, Row, Col } from 'react-bootstrap';
+import Wrapper from 'app/shared/components/Wrapper';
+import Container from '../../components/containers';
 
-export default class Settings extends React.Component {
+class Settings extends React.Component {
 	constructor(props){
 		super(props);
+
+		this.state = {
+			deleteModal: false
+		};
 	}
 
 	setMembers(members) {
@@ -13,66 +19,64 @@ export default class Settings extends React.Component {
 		});
 	}
 
+	toggleModal(state){
+		this.setState({deleteModal: state});
+	}
+
 	render() {
+
+
 		return (
-			<div className="content">
-				<Col md={2}>
-				</Col>
+			<div>
+				{/* Delete Project Modal */}
+				<Modal show={this.state.deleteModal} onHide={(e) => this.toggleModal(false)}>
+					<Modal.Header closeButton>
+						<Modal.Title>Delete Project?</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<p>Are you sure you want to delete </p>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={(e) => this.toggleModal(false)}>Close</Button>
+						<Button bsStyle="danger">Delete</Button>
+					</Modal.Footer>
+				</Modal>
 
-				<Col md={8}>
-					<div className="panel-group">
-						<div className="panel panel-primary">
-							<div className="panel-heading">
-								<h4>Settings </h4> </div>
+				<div className="content">
+					<Wrapper>
+						<div className="panel-group">
+							<div className="panel panel-primary">
+								<div className="panel-heading">
+									<h4>Project Settings</h4> 
+								</div>
 								<div className="panel-body">
-								<Row>
-										<Col md={3}>
-											<div className="form-group">
-												<label for="usr">Title</label>
-												<input type="text" className="form-control" id="usr" placeholder="change title"> </input>
-											</div>
-										</Col>
-
-											<Col md={9}>
-
-											</Col>
-
-
-
-
-
-									</Row>
-
-
-
-
-									<Row>
-
-										<Col md={5}>
 									<form>
-										<MultiSelect collection="users" labelKey="display_name" valueKey="_id" updateState={(members) => this.setMembers(members)}/>
+										<Row>
+											<Col md={6}>
+												<div className="form-group">
+													<label for="usr">Project Title</label>
+													<input type="text" className="form-control" id="usr" placeholder="Your Project Title"/>
+												</div>
+											</Col>
+											<Col md={6}>
+												<div className="form-group">
+													<label for="usr">Users</label>
+													<MultiSelect collection="users" labelKey="display_name" valueKey="_id" updateState={(members) => this.setMembers(members)}/>
+												</div>
+											</Col>
+										</Row>
 									</form>
-								</Col>
-
-
-									<Col md={7}>
-									</Col>
-
-
-
-									</Row>
-
-
 								</div>
 								<div className="panel-footer settings-foot">
-									<button type="save" className="btn btn-success pull-right save" data-toggle="modal" data-target="#myModal1"> Delete Project</button>
+									<Button type="button" bsStyle="danger" className="pull-right" onClick={(e) => this.toggleModal(true)}>Delete Project</Button>
 								</div>
 							</div>
-							<Col md={2}>
-							</Col>
 						</div>
-					</Col>
+					</Wrapper>
 				</div>
+			</div>
 		);
 	}
 }
+
+export default Container(Settings);
