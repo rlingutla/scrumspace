86d2acdca7701eeeb06975aaff6ee680364c92ee
Rachana Lingutla <rlingutla@umass.edu>
2016-04-02 13:30:21 -0400

    fixed another head issue

--- src/app/views/Statistics/components/GitGraphs2.js
+++ src/app/views/Statistics/components/GitGraphs2.js
@@ -12,12 +12,10 @@ const lineOptions = {
 };
 
 const GitGraphs2 = (props) => {
-<<<<<<< HEAD
 if (props.membersOnProj.length !== props.gCommits.length){
      props.gCommits.splice(props.membersOnProj.length);
 }
-=======
->>>>>>> 5cfb13578784fc629499ae0be9193a74c241f67f
+
 	const barData = {
 		labels: props.membersOnProj,
 		datasets: [
