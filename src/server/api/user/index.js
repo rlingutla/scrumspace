d0ecb9172f5f630b91aa70236ecba0c1f17605a9
Supriya Kankure <skankure@umass.edu>
2016-04-07 01:25:09 -0400

    added rest of attributes and added function for password

--- src/server/api/user/index.js
+++ src/server/api/user/index.js
@@ -57,19 +57,25 @@ router.put('/:user_id',  function(req, res) {
 // // gets users, and reads it
 var users =  readDocument ('users');
 
-console.log(users[userId].first_name);
-console.log(users[userId].last_name);
-console.log( users[userId].display_name);
-console.log(users[userId].email);
-console.log(users[userId].avatar_url);
-console.log(users[userId].password);
+// console.log(users[userId].first_name);
+// console.log(users[userId].last_name);
+// console.log( users[userId].display_name);
+// console.log(users[userId].email);
+// console.log(users[userId].avatar_url);
+// console.log(users[userId].password);
+
+ console.log(req.body.old_password);
+ console.log(users[userId].password);
+  console.log(req.body.new_password);
 
 /// see this **
-if(typeof req.body.password!=='undefined'){
-	if(req.body.password === users[userId].password){
-		users[userId].password  = req.body.password;
+if(typeof req.body.old_password!=='undefined'){
+	if(req.body.old_password === users[userId].password){
+		users[userId].password  = req.body.new_password;
 	}
 }
+
+ console.log(users[userId].password);
 
 if(typeof req.body.first_name!=='undefined') users[userId].first_name = req.body.first_name;
 if(typeof req.body.avatar_url!=='undefined') users[userId].avatar_url = req.body.avatar_url;
@@ -79,17 +85,12 @@ if(typeof req.body.display_name!=='undefined') users[userId].display_name = req.
 if(typeof req.body.email!=='undefined') users[userId].email= req.body.email;
 
 
-
-
-
-
-
-console.log(users[userId].first_name);
-console.log(users[userId].last_name);
-console.log( users[userId].display_name);
-console.log(users[userId].email);
-console.log(users[userId].avatar_url);
-console.log(users[userId].password);
+// console.log(users[userId].first_name);
+// console.log(users[userId].last_name);
+// console.log( users[userId].display_name);
+// console.log(users[userId].email);
+// console.log(users[userId].avatar_url);
+// console.log(users[userId].password);
 
 
  res.send(users[userId]);
