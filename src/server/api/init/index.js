912e4cc2ad61a25c695c5e4311051e2fea008337
Dylan Fischler <dylanfischler@gmail.com>
2016-04-15 14:07:48 -0400

    built that client normalize function

--- src/server/api/init/index.js
+++ src/server/api/init/index.js
@@ -22,57 +22,33 @@ module.exports = function (io, db) {
 				(user) => {
 					//check if auth user === user in param
 					if(req.params.user_id === user_id){
-						// packageProjects(user_id, db);
+						packageProjects(user_id, db).then(
+							(packagedProjects) => {
+								var stateTree = {
+									user: {
+											'_id': user._id,
+											'first_name': user.first_name,
+											'last_name': user.last_name,
+											'email': user.email,
+											'display_name': user.display_name,
+											'avatar_url': user.avatar_url
+									},
+									projects: packagedProjects
+								};
 
-						var projects = readDocument('projects');
-						var populatedProjects = projects.map((project) => {
-							return embedUsers(project);
-						}); 
-
-						var stateTree = {
-							user: {
-									'_id': user._id,
-									'first_name': user.first_name,
-									'last_name': user.last_name,
-									'email': user.email,
-									'display_name': user.display_name,
-									'avatar_url': user.avatar_url
+								res.send(stateTree);
 							},
-							projects: populatedProjects
-						};
-						res.send(stateTree);
+							(error) => res.status(500).send(error)
+						);
 					} else res.sendStatus(401);
 				},
 				(err) => {
-					console.log("err", err);
+					console.log('err', err);
 					if(err) res.sendStatus(500); //database error
 					else res.sendStatus(400); //user not found
 				}
 			);
 		});
-
-		
-
-		// var users = readDocument('users');
-
-		// var projects = readDocument('projects');
-		// var populatedProjects = projects.map((project) => {
-		// 	return embedUsers(project);
-		// }); 
-
-		// var user = users[userId];
-		// var stateTree = {
-		// 	user: {
-		// 			'_id': user._id,
-		// 			'first_name': user.first_name,
-		// 			'last_name': user.last_name,
-		// 			'email': user.email,
-		// 			'display_name': user.display_name,
-		// 			'avatar_url': user.avatar_url
-		// 	},
-		// 	projects: populatedProjects
-		// };
-		// res.send(stateTree);
 	});
 
 	return router;
